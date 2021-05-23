0bd900998b0e96830c1c587782d8b92530f6f593
buseozkan <buse1.ozkan@gmail.com>
2021-05-23 13:50:24 +0300

    registrationpage

--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import loginImg from "../login.svg";
+import './register.css';
 
+
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const validateForm = () => {
+    return email.length > 0 && password.length > 0;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="register">
+      <div>
+        <img src={loginImg} alt="img" />
+      </div>
+      <text id="text">Let's Sign Up </text>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label className="label">Name</Form.Label>
+          <Form.Control className="input"
+            autoFocus
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Surname</Form.Label>
+          <Form.Control className="input"
+            type="surname"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Email</Form.Label>
+          <Form.Control className="input"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Password</Form.Label>
+          <Form.Control className="input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Confirm Password</Form.Label>
+          <Form.Control className="input"
+            type="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          className="Registration-Button"
+          block
+          size="lg"
+          type="submit"
+          disabled={!validateForm()}
+        >
+          Register
+        </Button>
+      </Form>
+      <div className="Links">
+        <div>
+          <a className="continueAsGuest" href="ghost">
+            Just browse as a guest!
+          </a>
+        </div>
+        <div>
+          <a className="helpMe" href="btn">
+            Help Me
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+/*
 export class Register extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
@@ -38,16 +135,16 @@ export class Register extends React.Component {
         </div>
 
         {/*footer right-help
-        forget password*/}
-
-        <div className="footer">
-          <button type="button" className="btn">
-            Register
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+        forget password*/
+//{}
+//   <div className="footer">
+//   <button type="button" className="btn">
+//   Register
+// </button>
+//</div>
+//  </div>
+//);
+// }
+//}
 
 export default Register;
