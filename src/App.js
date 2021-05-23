a532e0d30cbd6279dcacc1b77fa1e18cb63fc6c5
Ahmet Mustafa <ahmet.m1993@gmail.com>
2021-05-23 21:57:32 +0300

    a few minor updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import Login from "../src/components/login";
-import Register from "../src/components/register";
-import forgetPassword from "../src/components/forgetpassword";
-import Programs from "../src/components/eventspage";
-import Home from "../src/components/homepage";
-import AboutUs from "../src/components/aboutus";
-import Search from "../src/components/search";
-import Navbar from "../src/components/navbar";
+import Login from "../src/components/Login/login";
+import Register from "../src/components/Register/register";
+import forgetPassword from "../src/components/ForgetPassword/forgetpassword";
+import Programs from "../src/components/EventsPage/eventspage";
+import Home from "../src/components/HomePage/homepage";
+import AboutUs from "../src/components/AboutUs/aboutus";
+import Navbar from "../src/components/NavBar/navbar";
 
 import {
   Route,
@@ -32,7 +31,6 @@ class App extends React.Component {
             <Route path="/register" component={Register}/>
             <Route path="/navbar" component={Navbar}/>
           </div>
-          <Search />
         </div>
       </HashRouter>
     )
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./navbar.css";
+import "../EventsPage/eventspage";
+import SearchBar from "../Search/search";
+
+const Navbar = () => {
+  return (
+    <div className="Navigation">
+      <SearchBar className="SearchBar" />
+      <NavLink
+        className="Navigation1"
+        to="/homepage"
+        activeStyle={{
+          color: "gold",
+        }}
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        className="Navigation1"
+        to="/aboutus"
+        activeStyle={{
+          color: "gold",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        className="Navigation1"
+        to="/eventspage"
+        activeStyle={{
+          color: "gold",
+        }}
+      >
+        Events
+      </NavLink>
+      <NavLink
+        className="Navigation1"
+        to="/login"
+        activeStyle={{
+          color: "gold",
+        }}
+      >
+        LogIn
+      </NavLink>
+      <NavLink
+        className="Navigation1"
+        to="/register"
+        activeStyle={{
+          color: "gold",
+        }}
+      >
+        Register
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
