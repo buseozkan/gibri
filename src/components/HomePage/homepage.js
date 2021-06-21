62d92ae2487e5021e537a9e3a171929e41fe2b3e
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-06-21 18:45:34 +0300

    /events

--- src/components/HomePage/homepage.js
+++ src/components/HomePage/homepage.js
@@ -14,7 +14,7 @@ const Programs = () => {
 
     const defatulCity = "Lefkoşa";
     const getEvent = async () => {
-        const response = await fetch("http://localhost:3000/events", {
+        const response = await fetch("http://localhost:3001/events", {
             method: 'GET',
             mode: 'cors',
             credentials: 'same-origin',
@@ -22,8 +22,9 @@ const Programs = () => {
                 'Content-Type': 'application/json'
             },
         });
-        const dat = await response.json()
-        setEvents(dat);
+        const data = await response.json()
+        setEvents(data.events);
+
     }
 
     React.useEffect(() => {
@@ -39,49 +40,19 @@ const Programs = () => {
                     </Link>
 
                 </Row>
-                <Dropdown className="citiesMenu" options={cities} onClick={() => {setCategory("Lefkoşa");
-                  }} placeholder="Select the city" />
+                <Dropdown className="citiesMenu" options={cities} onClick={() => {
+                    setCategory("Lefkoşa");
+                }} placeholder="Select the city" />
 
-                {/*
-                <select>
-{cities.map(city => <option>city</option>)}
-</select>
-                
-                */}
-                
             </div>
 
             <div class="boxes">
-                <div class="box">
-                    <img src="https://www.havadiskibris.com/wp-content/uploads/2019/06/Ahmet-Evan.jpg" />
-                    <h3>Ahmet Evan Narnia'da</h3><p>&nbsp;</p>
-                </div>
-                <div class="box">
-                    <img src="https://m.media-amazon.com/images/I/71gx-y3ksXL._SS500_.jpg" alt="Sunset in Amsterdam" />
-
-                    <h3>Frekans Shamrock'da</h3>
-                    <p> Detaylar icin.. </p>
-                </div>
-                <div class="box">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32795/pdx-signpost.jpg" alt="Sign in Pioneer Square; Portland, Oregon" />
-                    <h3>Portland Signpost</h3>
-                    <p> ... </p>
-                </div>
-                <div class="box">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32795/love-the-architecture-01-1024x768.jpg" alt="amsterdam architecture" />
-                    <h3>The architecture rocks</h3>
-                    <p> ... </p>
-                </div>
-                <div class="box">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32795/sunset-1024x768.jpg" alt="beautiful sunset" />
-                    <h3>Amsterdam Sunset</h3>
-                    <p> ... </p>
-                </div>
-                <div class="box">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32795/pdx-signpost.jpg" alt="Portland Signpost" />
-                    <h3>Portland Signpost</h3>
-                    <p> ... </p>
-                </div>
+                {events.map(event => {
+                    return (<div class="box">
+                        <img src="https://www.havadiskibris.com/wp-content/uploads/2019/06/Ahmet-Evan.jpg" />
+                        <h3>{event.eventName}</h3><p>&nbsp;</p>
+                    </div>)
+                })}
             </div>
         </>
     );
