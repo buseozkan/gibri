da9f01f4a841319a514270461ea0b62a03ed4e7b
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-06-21 23:40:05 +0300

    contact form

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -7,6 +7,7 @@ import {
     Link,
     HashRouter
 } from "react-router-dom";
+import contactUs from "../contactUs";
 
 class AboutUs extends React.Component {
     render() {
@@ -29,7 +30,9 @@ class AboutUs extends React.Component {
                     <button type="submit">Submit</button>
             </form>
             </div> */}
+            <contactUs></contactUs>
             </div>
+            
         )
     };
 };
