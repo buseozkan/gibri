dfb44b00af6637f9690bb8b1b8e1f247d5567c31
buseozkan <buse1.ozkan@gmail.com>
2021-07-10 10:26:20 +0300

    about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/style-prop-object */
 import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
@@ -16,9 +18,44 @@ const About = () => {
           the printing and typesetting industry. Lorem Ipsum has been the
           industry's standard dummy text ever since the 1500s
         </p>
-        <a href="/home" class="btn btn-primary">
+
+         {/* <a href="/home" class="btn btn-primary">
           View Date & Place <i class="fa fa-angle-right"></i>
-        </a>
+  </a> */}
+          <h2 class="Team">Our Team</h2>
+          <div class="row">
+            <div class="column">
+              <div class="card">
+               <img src="https://avatars.githubusercontent.com/u/74990420?v=4" alt="Akinsel" />
+               <div class="container">
+                 <h2>Ahmet Akinsel</h2>
+                 <p class="title">Junior Developer</p>
+               </div>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card">
+               <img src="https://avatars.githubusercontent.com/u/73131048?v=4" alt="Akinsel" />
+               <div class="container">
+                 <h2>Buse Ozkan</h2>
+                 <p class="title">Junior Developer</p>
+               </div>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card">
+               <img src="https://scontent.fada2-2.fna.fbcdn.net/v/t1.18169-9/29790177_10156371848822188_2342701848240893447_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeFwBrOeKQVn5Hlp3VywbnCBiSRkvATriHOJJGS8BOuIcxyG0W9LbzUXi_RvqBU2QK8&_nc_ohc=5nZtOa7Cw0wAX99A_L-&_nc_ht=scontent.fada2-2.fna&oh=b251868a74a89034243577dd646508e8&oe=60EE473C" alt="Akinsel" />
+               <div class="container">
+                 <h2>Ahmet Misirli</h2>
+                 <p class="title">Junior Developer</p>
+               </div>
+              </div>
+            </div>
+
+
+          </div>
       </div>
     </section>
   );
