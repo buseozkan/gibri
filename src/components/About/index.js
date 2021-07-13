54ff2b4d92ec5c860dd7c12e6f4bf2e283bc6419
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-07-13 20:50:39 +0300

    kkk

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,77 +4,55 @@ import "./about.css";
 
 const About = () => {
   return (
-    <section id="about">
-      <div className="container2">
-        <p>
-          We have created an events page as beginner junior developers. This
-          webpage is for Cyprus events only. Hope you will like it.
-        </p>
-        
-        <h2 class="Team">Our Team</h2>
-        <div className="row">
-          <div class="column">
-            <div className="kart">
-              <img
-                src="https://avatars.githubusercontent.com/u/74990420?v=4"
-                alt="Akinsel"
-              />
-              <div class="kutu">
-                <h2>Ahmet Akinsel</h2>
-                <p class="aciklama">Junior Developer</p>
-                <p>
-                  <form action="https://github.com/ahmetakinsel">
-                    <button type="link" class="github">
-                      Github Page
-                    </button>
-                  </form>
-                </p>{" "}
-              </div>
-            </div>
-          </div>
+    <div className="container2">
+      <h2 className="team-header">Our Team</h2>
+      <p className="page-text">
+        We have created an events page as beginner junior developers. This
+        webpage is for Cyprus events only. Hope you will like it.
+      </p>
+      <div className="team-members">
+        <div className="team-member-1">
+          <img
+            src="https://scontent.fada2-2.fna.fbcdn.net/v/t1.18169-9/29790177_10156371848822188_2342701848240893447_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeFwBrOeKQVn5Hlp3VywbnCBiSRkvATriHOJJGS8BOuIcxyG0W9LbzUXi_RvqBU2QK8&_nc_ohc=5nZtOa7Cw0wAX99A_L-&_nc_ht=scontent.fada2-2.fna&oh=b251868a74a89034243577dd646508e8&oe=60EE473C"
+            alt="Akinsel"
+          />
+          <h2 className="member-header-1">Ahmet Misirli</h2>
+          <p className="member-text">Junior Developer</p>
+          <form action="https://github.com/AhmetMustafa">
+            <button type="link" className="github-btn">
+              Github Page
+            </button>
+          </form>
+        </div>
+        <div className="team-member-2">
+          <img
+            src="https://avatars.githubusercontent.com/u/73131048?v=4"
+            alt="Akinsel"
+          />
+          <h2 className="member-header-2">Buse Ozkan</h2>
+          <p className="member-text">Junior Developer</p>
+          <form action="https://github.com/buseozkan">
+            <button type="link" className="github-btn">
+              Github Page
+            </button>
+          </form>
+        </div>
+        <div className="team-member-3">
+          <img
+            src="https://avatars.githubusercontent.com/u/74990420?v=4"
+            alt="Akinsel"
+          />
+          <h2 className="member-header-2">Ahmet Akinsel</h2>
+          <p className="member-text">Junior Developer</p>
 
-          <div class="column">
-            <div class="kart">
-              <img
-                src="https://avatars.githubusercontent.com/u/73131048?v=4"
-                alt="Akinsel"
-              />
-              <div class="kutu">
-                <h2>Buse Ozkan</h2>
-                <p class="aciklama">Junior Developer</p>
-                <p>
-                  <form action="https://github.com/buseozkan">
-                    <button type="link" class="github">
-                      Github Page
-                    </button>
-                  </form>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div class="kart">
-              <img
-                src="https://scontent.fada2-2.fna.fbcdn.net/v/t1.18169-9/29790177_10156371848822188_2342701848240893447_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeFwBrOeKQVn5Hlp3VywbnCBiSRkvATriHOJJGS8BOuIcxyG0W9LbzUXi_RvqBU2QK8&_nc_ohc=5nZtOa7Cw0wAX99A_L-&_nc_ht=scontent.fada2-2.fna&oh=b251868a74a89034243577dd646508e8&oe=60EE473C"
-                alt="Akinsel"
-              />
-              <div class="kutu">
-                <h2>Ahmet Misirli</h2>
-                <p class="aciklama">Junior Developer</p>
-                <p>
-                  <form action="https://github.com/AhmetMustafa">
-                    <button type="link" class="github">
-                      Github Page
-                    </button>
-                  </form>
-                </p>
-              </div>
-            </div>
-          </div>
+          <form action="https://github.com/ahmetakinsel">
+            <button type="link" className="github-btn">
+              Github Page
+            </button>
+          </form>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,29 +11,29 @@ const Home = () => {
 
   const cities = ["Lefkosa", "Girne", "Magusa", "Guzelyurt", "Iskele"];
   const artists = [
-  'Ahmet Evan',
-  'Rast',
-  'Dj Privillige',
-  'Eril Cambaz',
-  'Reva',
-  'SensiJam',
-  'Bunfyah',
-  'Fikri Karayel',
-  'Ezgi Akgurgen',
-  'Atesh',
-  'MishaRoots',
-  'Todds and Friends',
-  'Nafiz Dolek',
-  'Organic Pixel',
-  'Cahit Kutrafali Trio',
-  'Dj Coskuner',
-  'Ogulcan Basari',
-  'Dilara Ferit',
-  'Osman Tugsal',
-  'The Healers',
-  'Zildji',
-  'OneBlood',
-  'Osman Tugsal'
+    "Ahmet Evan",
+    "Rast",
+    "Dj Privillige",
+    "Eril Cambaz",
+    "Reva",
+    "SensiJam",
+    "Bunfyah",
+    "Fikri Karayel",
+    "Ezgi Akgurgen",
+    "Atesh",
+    "MishaRoots",
+    "Todds and Friends",
+    "Nafiz Dolek",
+    "Organic Pixel",
+    "Cahit Kutrafali Trio",
+    "Dj Coskuner",
+    "Ogulcan Basari",
+    "Dilara Ferit",
+    "Osman Tugsal",
+    "The Healers",
+    "Zildji",
+    "OneBlood",
+    "Osman Tugsal",
   ];
 
   const getEvent = async () => {
@@ -67,6 +67,8 @@ const Home = () => {
     });
   }
 
+  const defaultOption = "Cities";
+  const defaultOption2 = "Artists";
 
   return (
     <>
@@ -81,7 +83,7 @@ const Home = () => {
               console.log(citySelected.value, "target");
             }}
             placeholder="Sehrinizi Secin"
-            value={cities}
+            value={defaultOption}
           />
           <Dropdown
             className="dropdown1"
@@ -91,26 +93,22 @@ const Home = () => {
               console.log(artistSelected.value, "target");
             }}
             placeholder="Artist Secin"
-            value={cities}
+            value={defaultOption2}
           />
         </div>
-        <div>
-        </div>
+        <div></div>
         <div class="boxes">
           {eventsToShow.map((event) => {
             return (
               <>
                 <div class="box">
-                    <img src={event.image}></img>
-                    <h3>{event.eventName}</h3>
-                    <h2>{event.eventCity}</h2>
-                    <h2 className="unlu">{event.eventArtist}</h2>
-                    <h2>{event.eventImage}</h2>
-                    <p className="eventaciklama">{event.eventDescription}</p>
-                    <p className="eventaciklama">
-                      {" "}
-                      
-                    </p>
+                  <img src={event.image}></img>
+                  <h3>{event.eventName}</h3>
+                  <h2>{event.eventCity}</h2>
+                  <h2 className="unlu">{event.eventArtist}</h2>
+                  <h2>{event.eventImage}</h2>
+                  <p className="eventaciklama">{event.eventDescription}</p>
+                  <p className="eventaciklama"> </p>
                 </div>
                 <div></div>
               </>
